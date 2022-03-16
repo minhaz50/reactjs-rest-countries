fa8698d00ca9bd211a47a3641359ed746112affd
minhaz2597 <md.minhazuddin2597@gmail.com>
2022-03-17 00:34:23 +0600

    components folder added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
@@ -9,11 +10,34 @@ function App() {
   );
 }
 function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((respond) => respond.json())
+      .then((data) => setCountries(data));
+  }, []);
   return (
     <div>
       <h1>Visiting every country of the world!!!</h1>
+      <h3>Available countries: {countries.length}</h3>
+      {countries.map((country) => (
+        <p>
+          <Country
+            name={country.name.common}
+            continents={country.continents}
+          ></Country>
+        </p>
+      ))}
     </div>
   );
 }
 
+function Country(Props) {
+  return (
+    <div>
+      <h2>Name:{Props.name}</h2>
+      <h2>Continents:{Props.continents}</h2>
+    </div>
+  );
+}
 export default App;
